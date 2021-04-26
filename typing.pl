4913fe21e3d84833c2601bb417dcf31c5a163f5c
89oinotna <32968015+89oinotna@users.noreply.github.com>
2021-04-26 22:32:17 +0200

    Update typing.pl
    - fixed something (?)
    - added info to print

--- typing.pl
+++ typing.pl
@@ -1,7 +1,16 @@
+info(X, T) :-
+	write(X),
+    format(" : "),
+    write(T),
+    format("~n").
+
 ttype(_, X, _) :- atom(X).
 ttype(Γ,i(X), T) :-
-    nth0(_, Γ, (X, T)).
-ttype(_, N, int) :- number(N).
+    info(i(X), T),
+    nth0(_, Γ, (X, T)),
+    info(i(X), T).
+ttype(_, N, int) :- number(N),
+    info(N, int).
 ttype(Γ, op(_, E1, E2), int) :- 
     ttype(Γ,E1, int),
     ttype(Γ,E2, int).
@@ -17,13 +26,19 @@ ttype(Γ, fst(P), T) :-
 ttype(Γ, snd(P), T) :-
     ttype(Γ,P, (_,T)).
 ttype(Γ, abs(X, B), [TIde, TB]) :-
+    info(abs(X, B), [TIde, TB]),
     ttype(Γ, X, TIde),
-    ttype([(X, TIde)|Γ], B, TB).
+    ttype([(X, TIde)|Γ], B, TB),
+    info(abs(X, B), [TIde, TB]).
 ttype(Γ, app(E1, E2), T0) :-
+    info(app(E1, E2), T0),
+    ttype(Γ,E1, [T2, T0]),
     ttype(Γ,E2, T2),
-    ttype(Γ,E1, [T2, T0]).
+    info(app(E1, E2), T0).
 ttype(Γ, rec(X, B), T) :-
+    info(rec(X, B), T),
     ttype(Γ,X, T),
-    ttype([(X, T)|Γ], B, T).
+    ttype([(X, T)|Γ], B, T),
+    info(rec(X, B), T).
 ttype(X, T) :-
     ttype([], X, T).
