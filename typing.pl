530d536deb4b08fa2742d726b4f21f846dcfd4fa
89oinotna <32968015+89oinotna@users.noreply.github.com>
2021-04-26 12:36:18 +0200

    Update typing.pl

--- typing.pl
+++ typing.pl
@@ -1,24 +1,4 @@
-subst(_, _, n(Y), n(Y)).
-subst(T, X, i(X), T).
-subst(_, X, i(Y), i(Y)) :-
-    \+ X==Y.
-subst(T, X, op(O, E1, E2), op(O, R1, R2)) :-
-    subst(T, X, E1, R1),
-    subst(T, X, E2, R2).
-subst(T, X, if(C, E1, E2), if(R0, R1, R2)) :-
-	subst(T, X, C, R0),
-    subst(T, X, E1, R1),
-    subst(T, X, E2, R2).
-subst(T, X, pair(E1, E2), pair(R1, R2)) :-
-    subst(T, X, E1, R1),
-    subst(T, X, E2, R2).
-subst(T, X, fst(E), R) :-
-    subst(T, X, E, R).
-subst(T, X, snd(E), R) :-
-    subst(T, X, E, R).
-subst(T, X, app(E1, E2), app(R1, R2)) :-
-    subst(T, X, E1, R1),
-    subst(T, X, E2, R2).
+
 
 ttype(i(X), _) :- atom(X).
 ttype(n(X), int) :- number(X), write("int").
