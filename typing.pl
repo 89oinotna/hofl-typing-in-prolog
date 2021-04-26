c0da28aabee5b8d8968f387fb33f1e1bc00f0e46
89oinotna <32968015+89oinotna@users.noreply.github.com>
2021-04-26 14:09:56 +0200

    Update typing.pl
    added context

--- typing.pl
+++ typing.pl
@@ -1,29 +1,30 @@
-
-
-ttype(i(X), _) :- atom(X).
-ttype(n(X), int) :- number(X), write("int").
-ttype(op(_, E1, E2), int) :- 
-    ttype(E1, int),
-    ttype(E2, int).
-ttype(if(C, E1, E2), X) :-
-    ttype(C, int),
-    ttype(E1, X),
-    ttype(E2, X).
-ttype(pair(E1, E2), (X, Y)) :-
-    ttype(E1, X),
-    ttype(E2, Y).
-ttype(fst(P), X) :-
-    ttype(P, (X, _)).
-ttype(snd(P), Y) :-
-    ttype(P, (_,Y)).
-ttype(abs(X, B), [IDET, BT]) :-
+ttype(_, X, T) :- atom(X).
+ttype(Γ,i(X), T) :-
+    nth0(I, Γ, (X, T)).
+ttype(Γ, n(_), int).
+ttype(Γ, op(_, E1, E2), int) :- 
+    ttype(Γ,E1, int),
+    ttype(Γ,E2, int).
+ttype(Γ, if(C, E1, E2), X) :-
+    ttype(Γ,C, int),
+    ttype(Γ,E1, X),
+    ttype(Γ,E2, X).
+ttype(Γ, pair(E1, E2), (X, Y)) :-
+    ttype(Γ,E1, X),
+    ttype(Γ,E2, Y).
+ttype(Γ, fst(P), X) :-
+    ttype(Γ,P, (X, _)).
+ttype(Γ, snd(P), Y) :-
+    ttype(Γ,P, (_,Y)).
+ttype(Γ, abs(X, B), [IDET, BT]) :-
+    ttype(Γ, X, IDET),
+    ttype([(X, IDET)|Γ],B, BT).
+ttype(Γ, app(E0, E1), Y) :-
+    ttype(Γ,E1, X),
+    ttype(Γ,E0, [X, Y]).
+ttype(Γ, rec(X, B), T) :-
     atom(X),
-    ttype(X, IDET),
-    ttype(B, BT).
-ttype(app(E0, E1), Y) :-
-    ttype(E0, [X, Y]),
-    ttype(E1, X).
-ttype(rec(X, B), T) :-
-    atom(X),
-    ttype(X, T),
-    ttype(B, T).
+    ttype(Γ,X, T),
+    ttype(Γ,B, T).
+ttype(X, T) :-
+    ttype([], X, T).
