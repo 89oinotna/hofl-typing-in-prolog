22438cdfdf024b67d1a36af847e23152b73c4cc1
89oinotna <32968015+89oinotna@users.noreply.github.com>
2021-04-26 14:15:13 +0200

    Update typing.pl
    - numerical terms without n()
    - in rec added x in the context

--- typing.pl
+++ typing.pl
@@ -1,7 +1,7 @@
-ttype(_, X, T) :- atom(X).
+ttype(_, X, _) :- atom(X).
 ttype(Γ,i(X), T) :-
-    nth0(I, Γ, (X, T)).
-ttype(Γ, n(_), int).
+    nth0(_, Γ, (X, T)).
+ttype(_, N, int) :- number(N).
 ttype(Γ, op(_, E1, E2), int) :- 
     ttype(Γ,E1, int),
     ttype(Γ,E2, int).
@@ -23,8 +23,7 @@ ttype(Γ, app(E0, E1), Y) :-
     ttype(Γ,E1, X),
     ttype(Γ,E0, [X, Y]).
 ttype(Γ, rec(X, B), T) :-
-    atom(X),
     ttype(Γ,X, T),
-    ttype(Γ,B, T).
+    ttype([(X, T)|Γ],B, T).
 ttype(X, T) :-
     ttype([], X, T).
