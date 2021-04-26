8f4a618d52fb13aa980a669ea335b14e99e0b503
89oinotna <32968015+89oinotna@users.noreply.github.com>
2021-04-26 22:44:58 +0200

    Update typing.pl
    info on all terms

--- typing.pl
+++ typing.pl
@@ -9,22 +9,33 @@ ttype(Γ,i(X), T) :-
     info(i(X), T),
     nth0(_, Γ, (X, T)),
     info(i(X), T).
-ttype(_, N, int) :- number(N),
+ttype(_, N, int) :- 
+    number(N),
     info(N, int).
-ttype(Γ, op(_, E1, E2), int) :- 
+ttype(Γ, op(O, E1, E2), int) :-
+    info(op(O, E1, E2), int),
     ttype(Γ,E1, int),
-    ttype(Γ,E2, int).
+    ttype(Γ,E2, int),
+    info(op(O, E1, E2), int).
 ttype(Γ, if(C, E1, E2), T) :-
+    info(if(C, E1, E2), T),
     ttype(Γ,C, int),
     ttype(Γ,E1, T),
-    ttype(Γ,E2, T).
+    ttype(Γ,E2, T),
+    info(if(C, E1, E2), T).
 ttype(Γ, pair(E1, E2), (T1, T2)) :-
+    info( pair(E1, E2), T),
     ttype(Γ,E1, T1),
-    ttype(Γ,E2, T2).
+    ttype(Γ,E2, T2),
+    info( pair(E1, E2), T).
 ttype(Γ, fst(P), T) :-
-    ttype(Γ,P, (T, _)).
+    info(fst(P), T),
+    ttype(Γ,P, (T, _)),
+    info(fst(P), T).
 ttype(Γ, snd(P), T) :-
-    ttype(Γ,P, (_,T)).
+    info(snd(P), T),
+    ttype(Γ,P, (_,T)),
+    info(snd(P), T).
 ttype(Γ, abs(X, B), [TIde, TB]) :-
     info(abs(X, B), [TIde, TB]),
     ttype(Γ, X, TIde),
