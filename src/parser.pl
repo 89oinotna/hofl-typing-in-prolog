4e2d5480034ab979e362b55d22327d24da982ada
89oinotna <a.z.zegarelli@live.it>
2021-05-24 19:36:43 +0200

    problems with ()

--- src/parser.pl
+++ src/parser.pl
@@ -1,8 +1,10 @@
 :- module(parser, [parse/2]).
 
+preterm(X) --> expr(X).
 
+preterm(abs(X, B)) --> ["\\"], variable(X), ["."], preterm(B).
 
-preterm(abs(X, B)) --> ["\\"], variable(X), ["."], preterm(B),!.
+preterm(app(F, A)) --> preterm(F), expr(A).
 
 preterm(rec(X, B)) --> ["rec"], variable(X), ["."], preterm(B).
 
@@ -14,20 +16,11 @@ preterm(snd(Y)) --> ["snd("], preterm(Y), [")"].
 
 preterm(cond(C, B1, B2)) --> ["if"], preterm(C), ["then"], preterm(B1), ["else"], preterm(B2).
 
-preterm(var(X)) -->[Y], {atom_string(X, Y), \+number_string(_, Y)}.
-
-preterm(X) --> expr(X).
-
-preterm(app(F, A)) --> application(F), preterm(A).
-
-preterm([]) --> [].
-
-application(app(F, A)) --> preterm(F), preterm(A).
-
-variable(var(X)) --> [Y], {atom_string(X, Y), \+number_string(_, Y)}.
+variable(var(X)) --> [Y], {atom_string(X, Y), \+number_string(_, Y), \+member(Y, ['.', '+', '-', '*', '(', ')', '\\', ','])}.
 
 expr(bin_op(plus, L, R)) --> term(L), ["+"], expr(R).
 expr(bin_op(minus, L, R)) --> term(L), ["-"], expr(R).
+
 expr(X) --> term(X).
 
 term(bin_op(mul, L, R)) --> factor(L), ["*"], term(R).
@@ -37,9 +30,3 @@ term(X) --> factor(X).
 factor(X) --> ["("], preterm(X), [")"].
 factor(N) --> [I], {number_string(N, I)}.
 factor(V) --> variable(V).
-factor([]) --> [].
-
-
-
-
-
