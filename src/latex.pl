d225933d4c6d058a19fa41495b9d301f0525ae17
89oinotna <a.z.zegarelli@live.it>
2021-06-16 15:16:38 +0200

    - write latex
    - added tpair and tfun

--- src/latex.pl
+++ src/latex.pl
@@ -1,99 +1,151 @@
 :- module(latex, [write_latex/1]).
 
-write_latex(typed(abst(X, B), [TA | TB])) :- 
+write_latex(TT) :-
+    write_latex(0, TT).
+
+type_to_write( NTAU, T,  NTAU, T) :-
+    \+var(T).
+
+type_to_write( NTAU, T, RNTAU, tau(RNTAU)) :-
+    var(T),
+    RNTAU is NTAU +1,
+    T = tau(RNTAU).
+
+type_to_write( NTAU, tpair(T1, T2), RNTAU1, tpair(WT1, WT2)) :-
+    type_to_write( NTAU, T1, RNTAU, WT1),
+    type_to_write(RNTAU, T2, RNTAU1, WT2).
+
+type_to_write( NTAU, tfun(TA, TB), RNTAU1, tfun(WTA, WTB)) :-
+    type_to_write( NTAU, TA, RNTAU, WTA),
+    type_to_write(RNTAU, TB, RNTAU1, WTB).
+
+write_type(tfun(TA, TB)) :-
+    write_type(TA),
+    write(" \\rightarrow "),
+    write_type(TB).
+
+write_type(tpair(T1, T2)) :-
+    write("( "),
+    write_type(T1),
+    write(", "),
+    write_type(T2),
+    write(" )").
+
+write_type(tau(N)) :-
+    format("\\tau_{~p}", N).
+
+write_type(T) :-
+    write(T).
+
+write_latex(NTAU, typed(abst(X, B), tfun(TA, TB))) :- 
+    type_to_write(NTAU, TA, RNTAU, WTA),
+    type_to_write(RNTAU, TB, RNTAU1, WTB),
     write("\\underbracket{"),
         write("\\lambda"),
-        format("\\underbracket{~p}_{\\mathsf{~p}} . ", [X, TA]),
-        write_latex(B),
+        format("\\underbracket{~p}_{\\mathsf{", X),
+        write_type(WTA),
+        write("}} . "),
+        write_latex(RNTAU1, B),
         /*write("\\underbracket{"),
             write_latex(B),
             write("}_{\\mathsf{"),
             write(TB),
             write("}}"),*/
     write("}_{\\mathsf{"),
-    write([TA | TB]),
+    write_type( tfun(WTA, WTB)),
     write("}}").
 
-write_latex(typed(N, int)) :-
+write_latex( NTAU, typed(N, int)) :-
     number(N),
     format("\\underbracket{~d}_{\\mathsf{int}}", N).
 
-write_latex(typed(bin_op(OP, TT1, TT2),int)) :-
+write_latex( NTAU, typed(bin_op(OP, TT1, TT2),int)) :-
     write("\\underbracket{"),
-    write_latex(TT1),
+    write_latex( NTAU, TT1),
     write(OP),
-    write_latex(TT2),
+    write_latex( NTAU, TT2),
     write("}_{\\mathsf{int}}").
 
-write_latex(typed(cond(TTC, TT1, TT2), T)) :- 
-    write(" if \\underbracket{"),
-    write_latex(TTC),
+write_latex( NTAU,typed(cond(TTC, TT1, TT2), T)) :- 
+    type_to_write( NTAU, T, RNTAU, WT),
+    write("\\underbracket{ if "),
+    write_latex(RNTAU,TTC),
     write(" then "),
-    write_latex(TT1),
+    write_latex(RNTAU,TT1),
     write(" else "),
-    write_latex(TT2),
+    write_latex(RNTAU,TT2),
     write("}_{\\mathsf{"),
-    write(T),
+    write_type(WT),
     write("}}").
 
-write_latex(typed(pair(TT1, TT2), (T1, T2))) :-
+write_latex( NTAU,typed(pair(TT1, TT2), tpair(T1, T2))) :-
+    type_to_write( NTAU, T1, RNTAU, WT1),
+    type_to_write(RNTAU, T2, RNTAU1, WT2),
     write("\\underbracket{("),
-    write_latex(TT1),
+    write_latex(RNTAU1,TT1),
     write(", "),
-    write_latex(TT2),
+    write_latex(RNTAU1,TT2),
     write(")}_{\\mathsf{"),
-    write((T1, T2)),
+    write_type(tpair(WT1, WT2)),
     write("}}").
 
-write_latex(typed(fst(TT1), T1)) :-
+write_latex( NTAU,typed(fst(TT1), T1)) :-
+    type_to_write( NTAU, T1, RNTAU, WT1),
     write("\\underbracket{fst("),
-    write_latex(TT1),
+    write_latex(RNTAU, TT1),
     write(")}_{\\mathsf{"),
-    write(T1),
+    write_type(WT1),
     write("}}").
 
-write_latex(typed(snd(TT2), T2)) :-
+write_latex( NTAU,typed(snd(TT2), T2)) :-
+    type_to_write( NTAU, T2, RNTAU, WT2),
     write("\\underbracket{snd("),
-    write_latex(TT2),
+    write_latex(RNTAU,TT2),
     write(")}_{\\mathsf{"),
-    write(T2),
+    write_type(WT2),
     write("}}").
 
-write_latex(typed(app(TT, TT0), T1)) :-
+write_latex( NTAU,typed(app(TT, TT0), T1)) :-
+    type_to_write( NTAU, T1, RNTAU, WT1),
     write("\\underbracket{"),
-    write_latex(TT),
+    write_latex(RNTAU,TT),
     write(" "),
-    write_latex(TT0),
+    write_latex(RNTAU,TT0),
     write("}_{\\mathsf{"),
-    write(T1),
+    write_type(WT1),
     write("}}").
 
-write_latex(typed(rec(X, TT), T)) :-
+write_latex( NTAU, typed(rec(X, TT), T)) :-
+    type_to_write( NTAU, T, RNTAU, WT),
     write("\\underbracket{rec "),
-        format("\\underbracket{~p}_{\\mathsf{~p}} . ", [X, T]),
-        write("\\underbracket{"),
-            write_latex(TT),
+        format("\\underbracket{~p}_{\\mathsf{", X),
+        write_type(WT),
+        write("}} . "),
+        write_latex(RNTAU, TT),
+        /*write("\\underbracket{"),
+            write_latex(RNTAU, TT),
             write("}_{\\mathsf{"),
-            write(T),
-            write("}}"),
-    write(")}_{\\mathsf{"),
-    write(T),
+            write_type(T),
+            write("}}"),*/
+    write("}_{\\mathsf{"),
+    write_type(WT),
     write("}}").
 
-write_latex(typed(id(X), T)) :- 
+write_latex( NTAU,typed(id(X), T)) :- 
+    type_to_write( NTAU, T, RNTAU, WT),
     write("\\underbracket{"),
     write(X),
     write("}_{\\mathsf{"),
-    write(T),
+    write_type(WT),
     write("}}").
 
-write_latex(typed(Term, Type)) :-
+/*write_latex( NTAU,typed(Term, Type)) :-
     write("\\underbracket{"),
     write(Term),
     %write_latex(Term),
     write("}_{\\mathsf{"),
     write(Type),
-    write("}}").
+    write("}}").*/
 
 
 /*\[\underbracket{
--- src/typing.pl
+++ src/typing.pl
@@ -16,96 +16,25 @@ inferType(ENV, cond(C, E1, E2), T, typed(cond(TTC, TT1, TT2), T)) :-
     inferType(ENV, E1, T, TT1),
     inferType(ENV, E2, T, TT2).
 
-inferType(ENV, pair(E1, E2), (T1, T2), typed(pair(TT1, TT2), (T1, T2))) :-
+inferType(ENV, pair(E1, E2), tpair(T1, T2), typed(pair(TT1, TT2), tpair(T1, T2))) :-
     inferType(ENV, E1, T1, TT1),
     inferType(ENV, E2, T2, TT2).
 
 inferType(ENV, fst(E), T1, typed(fst(TT1), T1)) :-
-    inferType(ENV, E, (T1, _), TT1).
+    inferType(ENV, E, tpair(T1, _), TT1).
 
 inferType(ENV, snd(E), T2, typed(snd(TT2), T2)) :-
-    inferType(ENV, E, (_, T2), TT2).
+    inferType(ENV, E, tpair(_, T2), TT2).
 
-inferType(ENV, abst(X, B), [TA, TB], typed(abst(X, TTB), [TA, TB])) :-
+inferType(ENV, abst(X, B), tfun(TA, TB), typed(abst(X, TTB), tfun(TA, TB))) :-
     inferType([(X, TA)|ENV], B, TB, TTB).
 
 inferType(ENV, app(E1, E2), T1, typed(app(TT, TT0), T1)) :-
     inferType(ENV, E2, T0, TT0),
-    inferType(ENV, E1, [T0, T1], TT).
+    inferType(ENV, E1, tfun(T0, T1), TT).
 
 inferType(ENV, rec(X, B), T, typed(rec(X, TT), T)) :-
     inferType([(X, T)|ENV], B, T, TT).
 
 inferType(ENV, id(X), T, typed(id(X), T)) :- 
     nth0(_, ENV, (X, T)).
-/*.
-% entry point
-inferType(X, T) :-
-    %pre(term(X, tau(I)), T),
-    inferType([], term(X, tau(0)), T).%,
-    %post(term(X, tau(I)), T).
-
-inferType(ENV, term(id(X), tau(I)), T) :-
-    
-    nth0(_, ENV, (X, T, Y)),
-    pre(term(id(X), tau(I)), Y),
-    post(term(id(X), tau(I)), T).
-
-inferType(ENV, term(bin_op(O, E1, E2), tau(I)), int) :-
-    pre(term(bin_op(O, E1, E2), tau(I)), int),
-    I1 is I +1,
-    I2 is I1+1,
-    inferType(ENV,term(E1, tau(I1)), int),
-    inferType(ENV,term(E2, tau(I2)), int),
-    post(term(bin_op(O, E1, E2), tau(I)), int).
-
-inferType(ENV, term(if(C, E1, E2), tau(I)), T) :-
-    info(if(C, E1, E2), T),
-    inferType(ENV,term(C, tau(I)), int),
-    inferType(ENV,term(E1, tau(I)), T),
-    inferType(ENV,term(E2, tau(I)), T),
-    info(if(C, E1, E2), T).
-
-inferType(ENV, term(pair(E1, E2), tau(I)), (T1, T2)) :-
-    T=(T1, T2),
-    info( pair(E1, E2), T),
-    inferType(ENV,term(E1, T1), T1),
-    inferType(ENV, term(E2, T2), T2),
-    info( pair(E1, E2), T).
-
-inferType(ENV, term(fst(P), tau(I)), T) :-
-    info(fst(P), T),
-    inferType(ENV, term(P,(T,_)), (T, _)),
-    info(fst(P), T).
-
-inferType(ENV, term(snd(P), tau(I)), T) :-
-    info(snd(P), T),
-    inferType(ENV, term(P, (_,T)), (_,T)),
-    info(snd(P), T).
-
-inferType(ENV, term(abst(X, B), tau(I)), [TIde, TB]) :-
-    I1 is I+1,
-    I2 is I1+1,
-    pre(term(abst(X, B), tau(I)), [tau(I1), tau(I2)]),
-    inferType([(X, TIde, tau(I1))|ENV], term(B, tau(I2)), TB),
-    post(term(abst(X, B), [tau(I1), tau(I2)]), [TIde, TB]).
-
-inferType(ENV, term(app(E1, E2), tau(I)), T0) :-
-    info(app(E1, E2), T0),
-    inferType(ENV, term(E1, [T2, T0]), [T2, T0]),
-    inferType(ENV, term(E2, T2), T2),
-    info(app(E1, E2), T0).
-
-inferType(ENV, term(rec(X, B), tau(I)), T) :-
-    pre(term(rec(X, B), tau(I)), T),
-    inferType(ENV, term(X, tau(I)), T),
-    inferType([(X, T, tau(I))|ENV], term(B, tau(I)), T),
-    post(term(rec(X, B), tau(I)), T).
-
-inferType(ENV, term(N, tau(I)), T) :-
-    pre(term(N, tau(I)), int),
-    number(N),
-    post(term(N, int), int).
-
-inferType(_, term(X, _), _) :- atom(X).*/
-
